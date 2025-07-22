a107ad4983a646c6958cfc0a012cf521d85ebf40
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-21 23:49:55 -0600

    ArithmeticOperators problem

---
+++ Easy/ArithmeticOperators.py
@@ -0,0 +1,11 @@
+# Problem: Arithmetic Operators
+# Difficulty: Easy
+# Points: 10.00 points
+# URL: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print( a - b)
+    print(a * b)
--- Easy/SayHelloWorldWithPython.py
+++ Easy/SayHelloWorldWithPython.py
@@ -1,6 +1,6 @@
 # Problem: Say "Hello, World!" With Python
 # Difficulty: Easy
-# Points: 5.00 
+# Points: 5.00 points
 # URL: https://www.hackerrank.com/challenges/py-hello-world?isFullScreen=true
 
 if __name__ == '__main__':
