5e6e53a5679662e9fc829cb2f1f83a63b03b3c53
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-21 23:54:18 -0600

    PythonDivision problem

---
+++ Easy/PythonDivision.py
@@ -0,0 +1,10 @@
+# Problem: Python: Division
+# Difficulty: Easy
+# Points: 10.00 points
+# URL: https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a // b)
+    print(a / b)
