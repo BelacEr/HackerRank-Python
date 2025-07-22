af85e78d2eed935a9005b90f3ce41d9382cf78c6
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-21 23:19:01 -0600

    Easy problems

---
+++ Easy/SayHelloWorldWithPython.py
@@ -0,0 +1,7 @@
+# Problem: Say "Hello, World!" With Python
+# Difficulty: Easy
+# Points: 5.00 
+# URL: https://www.hackerrank.com/challenges/py-hello-world?isFullScreen=true
+
+if __name__ == '__main__':
+    print("Hello, World!")
