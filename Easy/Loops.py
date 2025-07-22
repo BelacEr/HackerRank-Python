dc7cb802167173c9ac7bbda2f014ee978d808b59
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-22 17:05:22 -0600

    Loops problem

---
+++ Easy/Loops.py
@@ -0,0 +1,9 @@
+# Problem: Loops
+# Difficulty: Easy
+# Points: 10.00 points
+# URL: https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i ** 2)
