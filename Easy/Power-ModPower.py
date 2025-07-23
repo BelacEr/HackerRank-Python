e128be5fe6c2fc9143bf133817fa1ea53f102a15
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-22 19:30:34 -0600

    Power-ModPower problem

---
+++ Easy/Power-ModPower.py
@@ -0,0 +1,12 @@
+# Problem: Power - Mod Power
+# Difficulty: Easy
+# Points: 10.00 points
+# URL: https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    m = int(input())
+    
+    print(pow(a, b))
+    print(pow(a, b, m))
